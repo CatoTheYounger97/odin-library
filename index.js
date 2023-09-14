a20c8478f7ff3a9ae19f188a1d0537d4a06e7570
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-09-15 00:50:35 +0100

    Refactor to use Classes

--- index.js
+++ index.js
@@ -1,97 +1,104 @@
-
-function Book(title, author, releaseDate, read) 
-{
-    this.title = title;
-    this.author = author;
-    this.releaseDate = releaseDate;
-    this.read = read;
-}
-
-function addBookToLibrary() 
-{
-    const title = document.querySelector('form > #title').value;
-    const author = document.querySelector('form > #author').value;
-    const releaseDate = document.querySelector('form > #release-date').value;
-    
-    myLibrary.push(new Book(title, author, releaseDate));
-}
-
-
-function displayBookOnPage()
-{   
-    // clear existing list, so no duplicates records are created
-    const bookList = document.querySelector('#book-list');
-    const bookRecords = Array.from(document.querySelectorAll('.BookRecord'));
-
-    for (let record in bookRecords) {
-        bookList.removeChild(bookRecords[record]);
+class Book {
+    constructor(title, author, releaseDate, read) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.read = read;
     }
-    
+}
 
+class Library {
+    library = [];
 
-    for (const book in myLibrary)
+    addBookToLibrary() 
     {
-
-        // div for book info card
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('BookRecord');
+        const title = document.querySelector('form > #title').value;
+        const author = document.querySelector('form > #author').value;
+        const releaseDate = document.querySelector('form > #release-date').value;
         
-        // present book data in an unordered list
-        const unorderedList = document.createElement('ul');
-        newDiv.appendChild(unorderedList);
-        
-        const listOne = document.createElement('li');
-        const listTwo = document.createElement('li');
-        const listThree = document.createElement('li');
-        const listFour = document.createElement('li');
+        this.library.push(new Book(title, author, releaseDate));
+    }
 
-        let bookObject = myLibrary[book];
+    displayBookOnPage()
+    {   
+        // clear existing list, so no duplicates records are created
+        const bookList = document.querySelector('#book-list');
+        const bookRecords = Array.from(document.querySelectorAll('.BookRecord'));
+    
+        for (let record in bookRecords) {
+            bookList.removeChild(bookRecords[record]);
+        }
         
-        listOne.innerText = 'Title: ' + bookObject.title;
-        listTwo.innerText = 'Author: ' + bookObject.author;
-        listThree.innerText = 'Release Date: ' + bookObject.releaseDate;
-        listFour.innerText = 'Read Status: ' + bookObject.read;
-        
-        unorderedList.appendChild(listOne);
-        unorderedList.appendChild(listTwo);
-        unorderedList.appendChild(listThree);
-        unorderedList.appendChild(listFour);
-        
-        // Add the remove button
-        const removeButton = document.createElement('button');
-        removeButton.innerText = 'remove';
-        removeButton.addEventListener('click', () => {
-            bookList.removeChild(newDiv);
-            myLibrary.splice(book, 1);
-            displayBookOnPage();
-        })
-        newDiv.appendChild(removeButton);
-
-        // Add read status button
-        const readStatusButton = document.createElement('button');
-        readStatusButton.innerText = 'Read Status';
-        readStatusButton.addEventListener('click', () => {
-            bookObject.read = !bookObject.read;
-            displayBookOnPage();
-        })
-        newDiv.appendChild(readStatusButton);
-
-
-        bookList.appendChild(newDiv);
+    
+    
+        for (const book in this.library)
+        {
+    
+            // div for book info card
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('BookRecord');
+            
+            // present book data in an unordered list
+            const unorderedList = document.createElement('ul');
+            newDiv.appendChild(unorderedList);
+            
+            const listOne = document.createElement('li');
+            const listTwo = document.createElement('li');
+            const listThree = document.createElement('li');
+            const listFour = document.createElement('li');
+    
+            let bookObject = this.library[book];
+            
+            listOne.innerText = 'Title: ' + bookObject.title;
+            listTwo.innerText = 'Author: ' + bookObject.author;
+            listThree.innerText = 'Release Date: ' + bookObject.releaseDate;
+            listFour.innerText = 'Read Status: ' + bookObject.read;
+            
+            unorderedList.appendChild(listOne);
+            unorderedList.appendChild(listTwo);
+            unorderedList.appendChild(listThree);
+            unorderedList.appendChild(listFour);
+            
+            // Add the remove button
+            const removeButton = document.createElement('button');
+            removeButton.innerText = 'remove';
+            removeButton.addEventListener('click', () => {
+                bookList.removeChild(newDiv);
+                this.library.splice(book, 1);
+                this.displayBookOnPage();
+            })
+            newDiv.appendChild(removeButton);
+    
+            // Add read status button
+            const readStatusButton = document.createElement('button');
+            readStatusButton.innerText = 'Read Status';
+            readStatusButton.addEventListener('click', () => {
+                bookObject.read = !bookObject.read;
+                this.displayBookOnPage();
+            })
+            newDiv.appendChild(readStatusButton);
+    
+    
+            bookList.appendChild(newDiv);
+        }
     }
 }
 
 
-const myLibrary = [];
+
+
+
+// const myLibrary = [];
+const myLibrary = new Library();
 
 // temp populate Library array with books 
-myLibrary.push(new Book('The Way of Kings', 'Brandon Sanderson', 2010, true));
-myLibrary.push(new Book('Words of Radiance', 'Brandon Sanderson', 2014, true));
-myLibrary.push(new Book('Oathbringer', 'Brandon Sanderson', 2017, false));
+myLibrary.library.push(new Book('The Way of Kings', 'Brandon Sanderson', 2010, true));
+myLibrary.library.push(new Book('Words of Radiance', 'Brandon Sanderson', 2014, true));
+myLibrary.library.push(new Book('Oathbringer', 'Brandon Sanderson', 2017, false));
 
-console.log(myLibrary);
+console.log(myLibrary.library);
 
-displayBookOnPage();
+myLibrary.displayBookOnPage();
 
 const newBookBtn = document.querySelector('#new-book-button');
 const formSubmit = document.querySelector('#submit-form');
@@ -114,6 +121,6 @@ formClose.addEventListener( 'click', (e) => {
 
 formSubmit.addEventListener( 'click', (e) => {
     e.preventDefault();
-    addBookToLibrary();
-    displayBookOnPage();
+    myLibrary.addBookToLibrary();
+    myLibrary.displayBookOnPage();
 });
